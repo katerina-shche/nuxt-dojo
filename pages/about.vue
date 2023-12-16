e04d88ffb90986e1ce723185fba0d6b9b5440d11
katerina-shche <asti.katerina@gmail.com>
2023-12-16 10:08:57 +0100

    Nutx Dojo Merch

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus neque dolorem sit aliquid sequi eos dolorum, excepturi ut. Veniam, rem.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum placeat obcaecati, eveniet architecto molestiae tempore soluta nulla! Earum, laborum explicabo.</p>
+    </div>
+</template>
+
+<script setup>
+    definePageMeta({
+        layout: 'default'
+    })
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
